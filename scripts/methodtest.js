ae078e185a4f9c418cc3c086f68b563bd98fce95
Tanmay Kulkarni <tanmay.n.kulkarni@hotmail.com>
2022-03-16 18:06:06 -0700

    post complete

--- scripts/methodtest.js
+++ scripts/methodtest.js
@@ -1,18 +1,62 @@
-function foobar() {
-    fetch("https://httpbin.org/get", {
-        method: "GET"
-      })
-      .catch(() => {
-        console.log("Fail zone");
-      })
-      .then((res) => {
-        if (res.ok) {
-          res.json().then((json) => {
-            document.getElementById("response").innerHTML = JSON.stringify(json, null, 2);
-          });
-        } else {
-          console.log("error", res);
+window.addEventListener("DOMContentLoaded", () => {
+
+    function get() {
+        fetch("https://httpbin.org/get", {
+            method: "GET"
+        })
+            .catch((e) => {
+                console.log(e);
+            })
+            .then((res) => {
+                if (res.ok) {
+                    res.json().then((json) => {
+                        console.log(res);
+                        document.getElementById("response").innerHTML = JSON.stringify(json, null, 4);
+                    });
+                } else {
+                    console.log("error", res);
+                }
+            });
+    }
+
+    function post() {
+
+        let data = {
+            id: _id.value,
+            articleName: _articleName.value,
+            articleBody: _articleBody.value
         }
-      });
-  }
-  
+
+        fetch("https://httpbin.org/post", {
+            method: "POST",
+            body: JSON.stringify(data),
+        })
+            .catch((e) => {
+                console.log(e);
+            })
+            .then((res) => {
+                if (res.ok) {
+                    res.json().then((json) => {
+                        console.log(res);
+                        document.getElementById("response").innerHTML = JSON.stringify(json, null, 4);
+                    });
+                } else {
+                    console.log("error", res);
+                }
+            });
+    }
+
+    let _id = document.getElementById("editId");
+    let _articleName = document.getElementById("editArticleName");
+    let _articleBody = document.getElementById("editArticleBody");
+
+    let getBtn = document.getElementById("getBtn");
+    let postBtn = document.getElementById("postBtn");
+    let putBtn = document.getElementById("putBtn");
+    let deleteBtn = document.getElementById("deleteBtn");
+
+    getBtn.addEventListener("click", get, true);
+    postBtn.addEventListener("click", post, true);
+    putBtn.addEventListener("click", post, true);
+    deleteBtn.addEventListener("click", post, true);
+});
